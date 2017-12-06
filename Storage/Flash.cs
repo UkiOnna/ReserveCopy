e37b72d289ee190df45251dc0a2c68964fac5800
UkiOnna <sikirovtemirlan@mail.ru>
2017-12-06 20:26:36 +0600

    ppc

---
+++ Storage/Flash.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage
+{
+    public class Flash:Storrage
+    {
+        private int speedUsb;
+        private int memory;
+        string[] data;
+
+        public override int GetMemory()
+        {
+            return memory;
+        }
+
+        public void  SetMemory(int memory)
+        {
+            this.memory = memory;
+        }
+
+        public int GetUsbSpeed()
+        {
+            return speedUsb;
+        }
+
+        public void SetSpeedUsb(int speedUsb)
+        {
+            this.speedUsb = speedUsb;
+        }
+
+        public override void CopyData(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Storage/Storrage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage
+{
+    public abstract class Storrage
+    {
+        private string _name;
+        private string _model;
+
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        public string GetModel()
+        {
+            return _model;
+        }
+
+        public void SetModel(string model)
+        {
+            _model = model;
+        }
+
+        public abstract int GetMemory();
+
+        public abstract void CopyData(string data);
+
+        public abstract void FreeMemoryInfo();
+
+        public abstract void GetInfo();
+    }
+}
